27b7251ebfade727ccecd1410b9b016d4e6e4d4a
oseunghyun <ohsh1477@gmail.com>
2023-04-15 17:46:37 +0900

    1. 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 사용자 정의 태그 -> 컴포넌트 만들기
+// function 키워드
+// 대문자 사용
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href='/'>WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href='/read/1'>html</a>
+        </li>
+        <li>
+          <a href='/read/2'>css</a>
+          <li>
+            <a href='/read/3'>js</a>
+          </li>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
