3e75d67ba77b5aa5307b58f929068d714f0e1e14
oseunghyun <ohsh1477@gmail.com>
2023-04-15 18:08:13 +0900

    2. props

--- src/App.js
+++ src/App.js
@@ -4,49 +4,54 @@ import './App.css';
 // 사용자 정의 태그 -> 컴포넌트 만들기
 // function 키워드
 // 대문자 사용
-function Header() {
+function Header(props) {
+  console.log('props', props.title);
   return (
     <header>
       <h1>
-        <a href='/'>WEB</a>
+        <a href='/'>{props.title}</a>
       </h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a href={'/read/' + t.id}>{t.title}</a>
+      </li>
+    );
+  }
   return (
     <nav>
-      <ol>
-        <li>
-          <a href='/read/1'>html</a>
-        </li>
-        <li>
-          <a href='/read/2'>css</a>
-          <li>
-            <a href='/read/3'>js</a>
-          </li>
-        </li>
-      </ol>
+      <ol>{lis}</ol>
     </nav>
   );
 }
 
-function Article() {
+function Article(props) {
   return (
     <article>
-      <h2>welcome</h2>
-      Hello, WEB
+      <h2>{props.title}</h2>
+      {props.body}
     </article>
   );
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is...' },
+    { id: 2, title: 'css', body: 'css is...' },
+    { id: 3, title: 'js', body: 'js is...' },
+  ];
   return (
     <div className='App'>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title='REACT'></Header>
+      <Nav topics={topics}></Nav>
+      <Article title='Welcome' body='hello, WEB'></Article>
     </div>
   );
 }
