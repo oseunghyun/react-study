a01884d877263af9b6aa3b328e04698b0acc70bf
oseunghyun <ohsh1477@gmail.com>
2023-04-15 22:31:34 +0900

    5. create

--- src/App.js
+++ src/App.js
@@ -61,6 +61,32 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type='text' name='title' placeholder='title' />
+        </p>
+        <p>
+          <textarea name='body' placeholder='body'></textarea>
+        </p>
+        <p>
+          <input type='submit' value='Create'></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   // const _mode = useState('WELCOME'); // 초기값
   // const mode = _mode[0]; // state의 값
@@ -68,12 +94,12 @@ function App() {
 
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'js', body: 'js is...' },
-  ];
+  ]);
 
   let content = null;
 
@@ -89,6 +115,20 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setMode('READ');
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
 
   return (
@@ -107,6 +147,15 @@ function App() {
         }}
       ></Nav>
       {content}
+      <a
+        href='/create'
+        onClick={(event) => {
+          event.preventDefault();
+          setMode('CREATE');
+        }}
+      >
+        Create
+      </a>
     </div>
   );
 }
