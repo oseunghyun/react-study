b15ff099457d46df6f555b57bbb828e267d9dce3
oseunghyun <ohsh1477@gmail.com>
2023-04-15 22:05:46 +0900

    4. state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 // 사용자 정의 태그 -> 컴포넌트 만들기
 // function 키워드
@@ -35,7 +36,7 @@ function Nav(props) {
           href={'/read/' + t.id}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
             // event를 유발시킨 타겟을 의미함 -> a태그
           }}
         >
@@ -61,26 +62,51 @@ function Article(props) {
 }
 
 function App() {
+  // const _mode = useState('WELCOME'); // 초기값
+  // const mode = _mode[0]; // state의 값
+  // const setMode = _mode[1]; // state의 값을 바꿀 때
+
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'js', body: 'js is...' },
   ];
+
+  let content = null;
+
+  if (mode === 'WELCOME') {
+    content = <Article title='Welcome' body='hello, WEB'></Article>;
+  } else if (mode === 'READ') {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
+
   return (
     <div className='App'>
       <Header
         title='REACT'
         onChangeMode={() => {
-          alert('Header');
+          setMode('WELCOME');
         }}
       ></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode('READ');
+          setId(_id);
         }}
       ></Nav>
-      <Article title='Welcome' body='hello, WEB'></Article>
+      {content}
     </div>
   );
 }
